470ffc81ab390e2541ad037202704c761c023343
Sergio Fidanli <gfidanli@gmail.com>
2022-10-30 16:39:31 -0500

    update etl process and db filepath

--- etl/load_database.py
+++ etl/load_database.py
@@ -13,7 +13,11 @@ Descriptions of the various datasets can be found in the project README.
 """
 
 # Create database connection
-conn = sqlite3.connect('../data/db/database.db')
+conn = sqlite3.connect('./data/db/database.db')
+
+# Season parameters
+season_start = 1999
+season_end = 2022
 
 try:
 
@@ -21,67 +25,67 @@ try:
     # Extract and Load into Database #
     # ============================== #
     
-    nfl.import_weekly_data(range(1999, 2022)).\
+    nfl.import_weekly_data(range(season_start, season_end+1)).\
         to_sql('weekly', conn, index=False, if_exists='replace')
 
-    nfl.import_seasonal_data(range(1999,2022)).\
+    nfl.import_seasonal_data(range(season_start,season_end+1)).\
         to_sql('seasonal', conn, index=False, if_exists='replace')
 
     nfl.import_team_desc().\
         to_sql('team_descriptions', conn, index=False, if_exists='replace')
 
-    nfl.import_rosters(range(1999,2022)).\
+    nfl.import_rosters(range(season_start,season_end+1)).\
         to_sql('rosters', conn, index=False, if_exists='replace')
 
-    nfl.import_win_totals(range(1999,2022)).\
+    nfl.import_win_totals(range(season_start,season_end+1)).\
         to_sql('win_totals', conn, index=False, if_exists='replace')
 
-    nfl.import_sc_lines(range(1999,2022)).\
+    nfl.import_sc_lines(range(season_start,season_end+1)).\
         to_sql('score_lines', conn, index=False, if_exists='replace')
 
-    nfl.import_officials(range(1999,2022)).\
+    nfl.import_officials(range(season_start,season_end+1)).\
         to_sql('officials', conn, index=False, if_exists='replace')
 
-    nfl.import_draft_picks(range(1999,2022)).\
+    nfl.import_draft_picks(range(season_start,season_end+1)).\
         to_sql('draft_picks', conn, index=False, if_exists='replace')
 
     nfl.import_draft_values().\
         to_sql('draft_values', conn, index=False, if_exists='replace')
 
-    nfl.import_combine_data(range(1999,2022)).\
+    nfl.import_combine_data(range(season_start,season_end+1)).\
         to_sql('combine', conn, index=False, if_exists='replace')
 
-    nfl.import_schedules(range(1999,2022)).\
+    nfl.import_schedules(range(season_start,season_end+1)).\
         to_sql('schedules', conn, index=False, if_exists='replace')
 
     nfl.import_ids().\
         to_sql('ids', conn, index=False, if_exists='replace')
 
-    nfl.import_ngs_data('passing', range(1999,2022)).\
+    nfl.import_ngs_data('passing', range(season_start,season_end+1)).\
         to_sql('ngs_pass', conn, index=False, if_exists='replace')
 
-    nfl.import_ngs_data('receiving', range(1999,2022)).\
+    nfl.import_ngs_data('receiving', range(season_start,season_end+1)).\
         to_sql('ngs_rec', conn, index=False, if_exists='replace')
 
-    nfl.import_ngs_data('rushing', range(1999,2022)).\
+    nfl.import_ngs_data('rushing', range(season_start,season_end+1)).\
         to_sql('ngs_rush', conn, index=False, if_exists='replace')
 
-    nfl.import_injuries(range(2009,2022)).\
+    nfl.import_injuries(range(2009,season_end+1)).\
         to_sql('injuries', conn, index=False, if_exists='replace')
 
-    nfl.import_qbr(range(2006,2022)).\
+    nfl.import_qbr(range(2006,season_end+1)).\
         to_sql('qbr', conn, index=False, if_exists='replace')
 
-    nfl.import_pfr('pass', range(2019,2022)).\
+    nfl.import_pfr('pass', range(2019,season_end+1)).\
         to_sql('pfr_pass', conn, index=False, if_exists='replace')
 
-    nfl.import_pfr('rec', range(2019,2022)).\
+    nfl.import_pfr('rec', range(2019,season_end+1)).\
         to_sql('pfr_rec', conn, index=False, if_exists='replace')
 
-    nfl.import_pfr('rush', range(2019,2022)).\
+    nfl.import_pfr('rush', range(2019,season_end+1)).\
         to_sql('pfr_rush', conn, index=False, if_exists='replace')
 
-    nfl.import_snap_counts(range(2012,2022)).\
+    nfl.import_snap_counts(range(2012,season_end+1)).\
         to_sql('snap_counts', conn, index=False, if_exists='replace')
 
     # ============================== #
